304dec3796f6ff1768ce14bfa9ab8f9a040b4141
Eric Regnier <eric.regnier.mail@gmail.com>
2022-02-19 01:03:41 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <GoogleMap
+      :api-key="apiKey"
+      style="width: 100%; height: 100%"
+      :center="center"
+      :zoom="15"
+  >
+    <gmap-custom-marker :marker="center">
+      <img height="75" alt="custom marker" src="https://cdn.rebrickable.com/media/thumbs/mocs/moc-91667.jpg/1000x800.jpg?1635782553.4022405"/>
+    </gmap-custom-marker>
+  </GoogleMap>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {GoogleMap} from 'vue3-google-map'
+import GmapCustomMarker from './gmap-custom-marker'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GoogleMap,
+    GmapCustomMarker
+  },
+  data() {
+    return {
+      center: {
+        lat: 50.6272265,
+        lng: 3.0571581
+      }
+    }
+  },
+  computed: {
+    apiKey() {
+      return process.env.VUE_APP_GMAP_API_KEY
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
 }
 </style>
